710dc9ecca0c280583e5526c6f5053d2d354c7aa
Dyrits <dyrits@outlook.fr>
2020-05-13 19:27:31 +0200

    Minor update.

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,9 @@ const workTimeLabel = document.querySelector("#worktime-label");
 const breakTimeRange = document.querySelector("#breaktime");
 const breakTimeLabel = document.querySelector("#breaktime-label");
 const tomatoLogo = document.querySelector("#logo");
+const countingDown = false;
 
 workTimeRange.onchange = () => workTimeLabel.innerHTML = workTimeRange.value;
 breakTimeRange.onchange = () => breakTimeLabel.innerHTML = breakTimeRange.value;
+
+
