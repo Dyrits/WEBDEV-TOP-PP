4465f3d4ebe1b62f93f20cefc77238b75b70ef31
Dyrits <dyrits@outlook.fr>
2020-05-13 20:16:31 +0200

    Minor update.

--- scripts/script.js
+++ scripts/script.js
@@ -7,22 +7,60 @@ const countingDown = {
   "work": false,
   "break": false
 };
+const timers = {
+  "work":  {
+    minutes: 25,
+    seconds: 0,
+    timerID: null,
+  },
+  "break": {
+    minutes: 5,
+    seconds: 0,
+    timerID: null,
+  }
+}
 
 workTimeRange.onchange = () => {
   workTimeLabel.innerHTML = workTimeRange.value;
-  updateTimer("work", workTimeRange.value);
+  updateTimer("work", "minutes", workTimeRange.value);
 }
+
 breakTimeRange.onchange = () => {
   breakTimeLabel.innerHTML = breakTimeRange.value;
-  updateTimer("break", breakTimeRange.value);
+  updateTimer("break", "minutes", breakTimeRange.value);
 }
 
 
-function updateTimer(workOrBreak, value) {
+function updateTimer(workOrBreak, minutesOrSeconds, value) {
   if (!countingDown[workOrBreak]) {
     if (value < 0) { value = 0; }
     if (value < 10) { value = "0" + value; }
-    document.querySelector("#" + workOrBreak + "-minutes").innerHTML = value || 0;
-    document.querySelector("#" + workOrBreak + "-seconds").innerHTML = "00";
+    if (minutesOrSeconds = "minutes") {
+      document.querySelector("#" + workOrBreak + "-minutes").innerHTML = value || 0;
+      document.querySelector("#" + workOrBreak + "-seconds").innerHTML = "00";
+    }
+    else if (minutesOrSeconds = "seconds") {
+      document.querySelector("#" + workOrBreak + "-seconds").innerHTML = value || 0;
+    }
   }
 }
+
+function startTimer(workOrBreak) {
+  countingDown[workOrBreak] = true;
+  let input = "#" + workOrBreak + "time";
+  timers[workOrBreak].timerID = setInterval(function () {
+    timers[workOrBreak].seconds -= 1;
+    if (timers[workOrBreak].seconds < 0) {
+      if (timers[workOrBreak] == 0) {
+        return stopTimer();
+      }
+      timers[workOrBreak].seconds = 59;
+      timers[workOrBreak].minutes--;
+    }
+
+    updateTimer(workOrBreak, "seconds", timers[workOrBreak].minutes);
+    updateTimer(workOrBreak, "minutes", timers[workOrBreak].seconds);
+
+  }, 1000)
+}
+
